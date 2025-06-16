f29d8fe888848f9bc82a6511c8ebd8a140c1a9e1
duanniten <duanniten>
2025-06-16 15:48:32 -0300

    create models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    categories = models.CharField(max_length = 30)
+
+
+class AuctionListings(models.Model):
+    title = models.CharField(max_length= 30)
+    description = models.TextField()
+    bid = models.PositiveIntegerField()
+    imageUrl = models.CharField(blank=True)
+    category = models.ForeignKey(Category, blank=True)
+
+
+class Bids(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
