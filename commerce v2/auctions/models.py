f3e8fdd81f07b9bdc557b6f5f37c33c8bf02c157
Duann Iten <duann_@hotmail.com>
2025-07-01 17:20:28 -0300

    create User Model (table)

--- commerce v2/auctions/models.py
+++ commerce v2/auctions/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
-    pass
+    watchList = models.ManyToManyField("Listing")
+    createdListing = models.ManyToManyField("Listing")
+    wonListing = models.ManyToManyField("Listing")
+    
