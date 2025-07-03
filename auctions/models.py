cbadf790eed9231499ff75231003133e76d65b26
duanniten <duanniten>
2025-07-03 14:28:14 -0300

    remake all models

--- auctions/models.py
+++ auctions/models.py
@@ -1,38 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class Category(models.Model):
-    categories = models.CharField(max_length = 30)
-    
-    def __str__(self):
-        return self.categories
-
-class AuctionListings(models.Model):
+class AuctionListing(models.Model):
     id = models.AutoField(primary_key=True)
+
     title = models.CharField(max_length= 30)
     description = models.TextField()
     imageUrl = models.CharField(blank= True, null= True)
-    category = models.ForeignKey(Category, blank= True, on_delete=models.SET_NULL, null= True)
-    creationTimestamp = models.DateTimeField(auto_now_add= True)
+    creationTime = models.DateTimeField(auto_now_add= True)
     timestamp = models.DateTimeField(auto_now = True)
+
+    category = models.ForeignKey("Category", blank= True, on_delete=models.SET_NULL, null= True)
     createUser = models.ForeignKey("User",on_delete=models.CASCADE)
+    biggerBid = models.ForeignKey("Bid", on_delete= models.SET_NULL) # mudar para procurar outra bid
+
     def __str__(self):
         return self.title
 
 class User(AbstractUser):
-    watchlist = models.ManyToManyField(AuctionListings,blank=True)
-    
-    
-class Bids(models.Model):
-    bidValue = models.DecimalField(max_digits=12, decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestand = models.DateTimeField(auto_now_add= True)
-    listing = models.ForeignKey(AuctionListings, on_delete= models.CASCADE)
+    watchlist = models.ManyToManyField(AuctionListing,blank=True)
 
+class Category(models.Model):
+    categories = models.CharField()
 
 class Comments(models.Model):
-    comment = models.TextField()
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    timestamp  = models.DateTimeField(auto_now_add= True)
-    listinig = models.ForeignKey(AuctionListings, on_delete=models.CASCADE)
+    listing = models.ForeignKey(AuctionListing, on_delete= models.CASCADE)
+    text = models.TextField(max_length = 500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits= 10, decimal_places= 2)
