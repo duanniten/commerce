60e9c237f4fbba2b823e4447b4d18f2a64a69d55
Duann Iten <duann_@hotmail.com>
2025-07-07 19:39:54 -0300

    implement change listing

--- commerce v2/auctions/forms.py
+++ commerce v2/auctions/forms.py
@@ -8,10 +8,29 @@ class CreateListing(ModelForm):
         max_digits=10, decimal_places=2, required=True
         )
 
-    class meta():
+    class Meta:
         models = models.Listing
         fields = [
             "imageUrl",
             "title",
             "description"
         ]
+
+class ChangeListing(ModelForm):
+    def __init__(self, bidValue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bidValue = bidValue + 0.01
+        self.fields['value'].widget.attrs.update({
+            "min" : self.bidValue,
+            "max" : self.bidValue
+        })
+    class Meta:
+        models = models.Bids
+        fields = [
+            "value"
+        ]
+        widgets = {
+            "value" : forms.NumberInput(attrs={
+                "class" : "form-control"
+            })
+        }
